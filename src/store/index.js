fefa12351184f866c0adf957c8f3cc12e396006b
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-04-10 14:27:08 +0000

    Admin: Add Server

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,16 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     clicked_game:null,
+    clicked_server:null
   },
   getters: {
   },
   mutations: {
     change_game(state , payload){
       state.clicked_game = payload
+    },
+    change_server(state , payload){
+      state.clicked_server = payload
     }
   },
   actions: {
