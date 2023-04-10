2a87ab1ae56206bc129e511ee64aa80b907b6fae
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-04-10 14:19:23 +0000

    Admin: Fetch Server & Search option

--- src/views/Admin/Servers/Servers.vue
+++ src/views/Admin/Servers/Servers.vue
@@ -1,3 +1,193 @@
 <template>
     <h1>Show Servers</h1>
-</template>
+    <router-link :to="{name : 'AddServer'}"><button class="Add-Button">Add Server</button></router-link>
+    <div class="Sheach">
+      <input type="text" name="" id="" v-model="looking_for">
+      <button @click="Clear">X</button>
+    </div>
+    <div class="Servers" v-if="servers.length">
+      <div class="Server" v-for="(server, key) in filtredServers" :key="key">
+          <div class="Buttons">
+              <button class="Delete" @click="delete_server(server.id)">Delete</button>
+              <button class="Update" @click="update_server(server)">Update</button>
+          </div>
+          <div class="Image">
+              <img :src="'http://localhost/RPG_World_Laravel/public/uploads/games/'+server.image" alt="">
+          </div>
+          <div class="Name">
+              <p>{{ server.name }}</p>
+          </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+      export default{
+          created(){
+            this.fetch_servers()
+          },
+          data(){
+            return {
+              servers:{},
+              looking_for:''
+            }
+          },
+          methods:{
+            fetch_servers(){
+              axios.get('http://127.0.0.1:8000/api/V1/servers')
+                .then((responce) => this.servers = responce.data.servers)
+            },
+            delete_server(server_id){
+              confirm('Are You Sure')
+              axios.delete('http://127.0.0.1:8000/api/V1/servers/' + server_id)
+                .then(() => {
+                  this.fetch_servers();
+                })
+              
+            },
+            update_server(server){
+              this.$store.commit('change_server', server)
+              this.$router.push({name : 'UpdateServer'})
+            },
+            Clear(){
+              this.looking_for = ''
+            }
+          },
+          computed:{
+            filtredServers(){
+              if(this.servers) return this.servers.filter(server => server.name.toLowerCase().includes(this.looking_for.toLowerCase()))
+          }
+      }
+      }
+  </script>
+  
+  <style lang="scss" scoped>
+  
+  .Add-Button{
+    color: orange;
+    background-color: white;
+    border: 1px solid orange;
+    border-radius: 5px;
+    padding: 5px 10px;
+    font-size: 14px;
+    cursor: pointer;
+  
+    margin-bottom: 10px;
+    
+    &:hover {
+      background-color: orange;
+      color: white;
+    }
+  }
+  
+  .Servers {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    margin: 20px;
+    
+    .Server {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin: 10px;
+      
+      .Buttons {
+        position: absolute;
+        top: 0;
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        padding: 10px;
+        
+        .Delete {
+          color: red;
+          background-color: white;
+          border: 1px solid red;
+          border-radius: 5px;
+          padding: 5px 10px;
+          font-size: 14px;
+          cursor: pointer;
+          
+          &:hover {
+            background-color: red;
+            color: white;
+          }
+        }
+        
+        .Update {
+          color: blue;
+          background-color: white;
+          border: 1px solid blue;
+          border-radius: 5px;
+          padding: 5px 10px;
+          font-size: 14px;
+          cursor: pointer;
+          
+          &:hover {
+            background-color: blue;
+            color: white;
+          }
+        }
+      }
+      
+      .Image {
+        width: 200px;
+        height: 200px;
+        overflow: hidden;
+        border-radius: 50%;
+        margin-bottom: 10px;
+        
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+      }
+      
+      .Name {
+        text-align: center;
+        font-size: 20px;
+        font-weight: bold;
+      }
+    }
+  }
+  
+  /* Style for the search bar */
+  .Sheach {
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+  
+    input {
+      flex: 1;
+      padding: 10px;
+      border-radius: 5px;
+      border: 1px solid #ccc;
+      font-size: 16px;
+    }
+  
+    button {
+      margin-left: 10px;
+      padding: 10px;
+      border-radius: 5px;
+      border: none;
+      background-color: #ccc;
+      color: #fff;
+      font-size: 16px;
+      cursor: pointer;
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
+    
+  </style>
