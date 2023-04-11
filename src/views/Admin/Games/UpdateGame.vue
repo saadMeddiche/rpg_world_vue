5472a0933a06763fd98ef42205bbce4a7b64bea8
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-04-11 09:05:54 +0000

    Add The Button To some pages

--- src/views/Admin/Games/UpdateGame.vue
+++ src/views/Admin/Games/UpdateGame.vue
@@ -1,5 +1,8 @@
 <template>
-    <h1>Update Game</h1>
+    <div class="Title">
+        <h1>Update Game</h1>
+        <backbutton></backbutton>
+    </div>
     <div class="Errors">
         <div class="Error" v-for="(error , key) in errors" :key="key">
             <p> {{ error[0] }}</p>
@@ -26,8 +29,13 @@
 
 <script>
 import axios from 'axios'
+import backbutton from '@/components/backbutton.vue'
+
 
     export default{
+        components:{
+            backbutton,
+        },
         data(){
             return {
                 game:{
@@ -81,6 +89,13 @@ import axios from 'axios'
 </script>
 
 <style lang="scss" scoped>
+
+.Title{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap:10px;
+}
 .Success_Message {
 
 background-color: rgba(0, 255, 0, 0.8);
--- src/views/Admin/Servers/UpdateServer.vue
+++ src/views/Admin/Servers/UpdateServer.vue
@@ -1,5 +1,9 @@
 <template>
-    <h1>Update Server</h1>
+
+    <div class="Title">
+        <h1>Update Server</h1>
+        <backbutton></backbutton>
+    </div>
     <div class="Errors">
         <div class="Error" v-for="(error , key) in errors" :key="key">
             <p> {{ error[0] }}</p>
@@ -31,8 +35,13 @@
 
 <script>
 import axios from 'axios'
+import backbutton from '@/components/backbutton.vue'
+
 
     export default{
+        components:{
+            backbutton,
+        },
         data(){
             return {
                 server:{
@@ -92,6 +101,13 @@ import axios from 'axios'
 </script>
 
 <style lang="scss" scoped>
+
+.Title{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap:10px;
+}
 .Success_Message {
 
     background-color: rgba(0, 255, 0, 0.8);
