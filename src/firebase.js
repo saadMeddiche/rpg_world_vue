cdcba44132705d8ec9a92c5e323daf04398008c5
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-04-11 18:01:05 +0000

    Sync

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+//https://www.youtube.com/watch?v=72Fk9i9HcZM
+
+import test from 'firebase/compat/app' ;
+import 'firebase/compat/database';
+
+
+const database = test.initializeApp({
+    
+    apiKey: "AIzaSyBQdsly58SmLXkgzEa47PtijYVPox9_3-U",
+    authDomain: "rpg-world-9c5dc.firebaseapp.com",
+    projectId: "rpg-world-9c5dc",
+    storageBucket: "rpg-world-9c5dc.appspot.com",
+    messagingSenderId: "176562738080",
+    appId: "1:176562738080:web:ccb6b4bc754187fdd74ac5"
+    
+})
+
+export default database;
--- src/views/Servers/Server.vue
+++ src/views/Servers/Server.vue
@@ -1,7 +1,26 @@
 <template>
     <h1>{{ server.name }}</h1>
+    <h4>{{ message_in_field }}</h4>
+    <div class="BoxOfChat">
+        <div class="Message">
+            <div class="Sender">
+                <p>Saad Meddiche</p>
+            </div>
+            <div class="Content">
+                <p>Hiii,Lucas How are You my Friend</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="FieldOfMessage">
+        <input type="text" v-model="message_in_field">
+        <button @click="send_message">send</button>
+    </div>
+
 </template>
 <script>
+    import firebase from '@/firebase.js';
+
     export default{
         created(){
             this.get_server()
@@ -9,16 +28,78 @@
         data(){
             return{
                 server:'',
+                message_in_field:''
             }
         },
         methods:{
             get_server(){
                 this.server = this.$store.state.clicked_server
+            },
+            send_message(){
+                let messageRef = firebase.database().ref("messages")
+
+                if(this.message_in_field == '' || this.message_in_field == null){
+                    return;
+                }
+
+                const message = {
+                    username:'Saad Meddiche',
+                    content:this.message_in_field
+                }
+
+                messageRef.push(message)
+
+                this.message_in_field=""
             }
         }
     }
 </script>
 
-<style>
+<style lang="scss" scoped>
+$primary-color: #0080ff;
+$secondary-color: #f2f2f2;
+
+.BoxOfChat {
+  border: 1px solid $primary-color;
+  padding: 10px;
+  background-color: $secondary-color;
+
+  .Message {
+    margin-bottom: 10px;
+
+    .Sender {
+      font-weight: bold;
+    }
+  }
+}
+
+.FieldOfMessage {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20px;
+
+  input[type="text"] {
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    flex: 1;
+    margin-right: 10px;
+  }
+
+  button {
+    background-color: $primary-color;
+    color: white;
+    border: none;
+    padding: 10px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.2s ease-in-out;
+
+    &:hover {
+      background-color: darken($primary-color, 10%);
+    }
+  }
+}
 
 </style>
