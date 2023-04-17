f6f24c73e084390dd26ed7b5a1ce99371d8640e9
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-04-17 12:40:55 +0000

    Style Of The Loader

---
+++ src/components/loading.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="Circles">
+      <div class="Circle Num1"></div>
+      <div class="Circle Num2"></div>
+      <div class="Circle Num3"></div>
+    </div>
+</template>
+  
+<!-- css animation - loding  -->
+<!-- https://www.w3schools.com/css/css3_animations.asp -->
+<style lang="scss" scoped>
+    .Circles {
+        display: flex;
+
+        justify-content: center;
+        align-items: center;
+
+        gap:4px;
+
+        
+    }
+    .Circle{
+
+        width: 8px;
+        height: 8px;
+
+        border-radius: 50%;
+
+        background-color: #de424d;
+        animation: ellipsis 10s infinite;
+    }
+  
+    //Loop in scss
+    //https://sass-lang.com/documentation/at-rules/control/for
+    @for $i from 1 through 3{
+        .Num#{$i} {
+            animation-delay: #{$i / 10}s;
+        }
+    }
+  
+  // Key Frame Rule for ellipsis
+  @keyframes ellipsis {
+    0% {
+      opacity: 0.2;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+</style>
+  
--- src/views/Admin/Dashboard.vue
+++ src/views/Admin/Dashboard.vue
@@ -3,13 +3,17 @@
    <p>Games :{{ count_games }}</p>
    <p>Servers :{{ count_servers }}</p>
    <p>Users :{{ count_users }}</p>
+   <loading></loading>
    <router-link :to="{name :'home'}">Home</router-link>
 </template>
 
 <script>
    import {count} from '@/utils/apiFunctions'
-
+   import loading from '@/components/loading.vue'
    export default{
+      components:{
+         loading,
+      },
       mounted(){
          count('count_games',this)
          count('count_servers',this)
